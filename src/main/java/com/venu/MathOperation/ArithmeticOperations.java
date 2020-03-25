87b8c7e3a93c53d1b831cc6b78329b16125c268c
venu <kvenu.reddy1@googlemail.com>
2020-03-25 11:42:32 +0530

    sonarcloud test

---
+++ src/main/java/com/venu/MathOperation/ArithmeticOperations.java
@@ -0,0 +1,10 @@
+package com.venu.MathOperation;
+
+public class ArithmeticOperations {
+
+	public Integer add(Integer a, Integer b)
+	{
+		return a+b;
+	}
+
+}
---
+++ src/test/java/com/venu/MathOperation/ArithmeticOperationsTest.java
@@ -0,0 +1,21 @@
+package com.venu.MathOperation;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class ArithmeticOperationsTest {
+	@Test
+	public void testAdd()
+	{
+	
+		ArithmeticOperations operations = new ArithmeticOperations();
+		Integer actual = operations.add(2, 6);
+		Integer expected = 8;
+		assertEquals(expected, actual);
+		
+	}
+
+
+}
